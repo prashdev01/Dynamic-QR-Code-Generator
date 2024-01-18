43ddbc12cb546a22890a0cfd1f310806496470d0
Prashant wadhave <125750774+prashdev01@users.noreply.github.com>
2024-01-18 16:29:51 +0530

    project done!

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 from flask import Flask,render_template ,request ,send_file 
 import qrcode as qr
+from flask import Flask, render_template, request
+from flask_pymongo import pymongo
+from urllib.parse import quote_plus
+import pymongo
 
 app = Flask(__name__)
+
+
+
+client =pymongo.MongoClient("mongodb+srv://prashantwadhave5:Prash2002@cluster0.wkfocs3.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+db = client.DYNAMIC_QR_GENERATOR  # Use your actual database name
+collection = db['contact_list'] 
+
+@app.route('/submit_contact_form', methods=['POST'])
+def submit_contact_form():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        print(f"Name: {name}, Email: {email}, Message: {message}")
+
+        # Store the form data in MongoDB
+        collection.insert_one({
+            'name': name,
+            'email': email,
+            'message': message
+        })
+
+        return  render_template('formsub.html')
+    else:
+        return "Method Not Allowed", 405
 
 @app.route('/')
 def index():
@@ -21,6 +51,7 @@ def generate_qr():
     return render_template('qr_gen.html')
 # Disable caching for development
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
     
 if __name__ == ('__main__'):
     app.run(debug=True)
